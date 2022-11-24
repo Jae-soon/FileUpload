44ceacfd1ecab3c89202395fc4dac96092e6d14b
Jae-Soon Kwon <91954574+Jae-soon@users.noreply.github.com>
2022-11-25 05:08:48 +0900

    Setting

---
+++ src/test/java/com/ll/re_fileupload/ReFileUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.re_fileupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReFileUploadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
