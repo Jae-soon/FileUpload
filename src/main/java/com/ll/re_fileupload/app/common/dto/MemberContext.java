bab1c752a0e8031c86f55b8f739538cc92ca2891
Jae-Soon Kwon <91954574+Jae-soon@users.noreply.github.com>
2022-12-07 01:34:47 +0900

    Feat : Principal -> MemberContext 변경, JSON형태 출력

---
+++ src/main/java/com/ll/re_fileupload/app/common/dto/MemberContext.java
@@ -0,0 +1,20 @@
+package com.ll.re_fileupload.app.common.dto;
+
+import com.ll.re_fileupload.app.member.entity.Member;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.List;
+
+@Getter
+public class MemberContext extends User {
+    private final Long id;
+    private final String profileImgUrl;
+
+    public MemberContext(Member member, List<GrantedAuthority> authorities) {
+        super(member.getUsername(), member.getPassword(), authorities);
+        this.id = member.getId();
+        this.profileImgUrl = member.getProfileImgUrl();
+    }
+}
--- src/main/java/com/ll/re_fileupload/app/home/controller/HomeController.java
+++ src/main/java/com/ll/re_fileupload/app/home/controller/HomeController.java
@@ -1,12 +1,16 @@
 package com.ll.re_fileupload.app.home.controller;
 
+import com.ll.re_fileupload.app.common.dto.MemberContext;
 import com.ll.re_fileupload.app.member.entity.Member;
 import com.ll.re_fileupload.app.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.security.Principal;
 
@@ -23,5 +27,18 @@ public class HomeController {
     @GetMapping("/about")
     public String showAbout() {
         return "home/about";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/currentUserOrigin")
+    @ResponseBody
+    public Principal currentUserOrigin(Principal principal) {
+        return principal;
+    }
+
+    @GetMapping("/currentUser")
+    @ResponseBody
+    public MemberContext currentUser(@AuthenticationPrincipal MemberContext memberContext) { // 로그인된 회원 -> MemberContext
+        return memberContext;
     }
 }
