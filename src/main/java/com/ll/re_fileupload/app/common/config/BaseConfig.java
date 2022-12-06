bd6a508ed17d7a39ce377bb87e525db116fbecf8
Jae-Soon Kwon <91954574+Jae-soon@users.noreply.github.com>
2022-12-07 00:17:23 +0900

    Feat : 회원 이미지 삭제

--- src/main/java/com/ll/re_fileupload/app/common/config/BaseConfig.java
+++ src/main/java/com/ll/re_fileupload/app/common/config/BaseConfig.java
@@ -1,6 +1,7 @@
 package com.ll.re_fileupload.app.common.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,4 +13,11 @@ public class BaseConfig {
     public JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
         return new JPAQueryFactory(entityManager);
     }
+
+    public static String GET_FILE_DIR_PATH;
+
+    @Value("${custom.genFileDirPath}")
+    public void setFileDirPath(String genFileDirPath) {
+        GET_FILE_DIR_PATH = genFileDirPath;
+    };
 }
--- src/main/java/com/ll/re_fileupload/app/member/entity/Member.java
+++ src/main/java/com/ll/re_fileupload/app/member/entity/Member.java
@@ -1,11 +1,13 @@
 package com.ll.re_fileupload.app.member.entity;
 
+import com.ll.re_fileupload.app.common.config.BaseConfig;
 import com.ll.re_fileupload.app.common.entity.BaseEntity;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import java.io.File;
 
 @Entity
 @Setter
@@ -20,4 +22,16 @@ public class Member extends BaseEntity {
     private String password;
     private String email;
     private String profileImg;
+
+    public void removeProfileImgOnStorage() {
+        if (profileImg == null || profileImg.trim().length() == 0) return;
+
+        String profileImgPath = getProfileImgPath();
+
+        new File(profileImgPath).delete();
+    }
+
+    private String getProfileImgPath() {
+        return BaseConfig.GET_FILE_DIR_PATH + "/" + profileImg;
+    }
 }
